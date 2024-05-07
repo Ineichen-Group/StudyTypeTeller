6afe1e28cf34e9ac329d8b5f5a8bf31a169e693a
shtosti <hanna.hubarava@gmail.com>
2024-05-07 12:58:21 +0200

    add true class names to x ticks

--- scripts/data/plot_class_distributions_in_splits.py
+++ scripts/data/plot_class_distributions_in_splits.py
@@ -33,22 +33,22 @@ def plot_class_distribution(train_data, val_data, test_data, label_column, save_
     axs[2].set_title('Test Data Class Distribution')
     axs[2].set_xlabel('Class')
     axs[2].set_ylabel('Count')
-
     plt.tight_layout()
 
     # save plots
     os.makedirs(save_dir, exist_ok=True)
     plt.savefig(f"{save_dir}/class_distribution_{label_column}.png")
+
     plt.show()
 
 
 def main():
     # Path to train, val, and test data files
-    train_file = "./../../data/data_splits_stratified/6-2-2_all_classes/train.csv"
-    val_file = "./../../data/data_splits_stratified/6-2-2_all_classes/val.csv"
-    test_file = "./../../data/data_splits_stratified/6-2-2_all_classes/test.csv"
-
-    save_dir = "./../../data/data_splits_stratified/6-2-2_all_classes/plots"
+    filepath = './../../data/data_splits_stratified/6-2-2_all_classes_enriched_with_kw/'
+    train_file = f"{filepath}train.csv"
+    val_file = f"{filepath}val.csv"
+    test_file = f"{filepath}test.csv"
+    save_dir = f"{filepath}plots"
 
     # Read train, val, and test data
     train_data = pd.read_csv(train_file)
@@ -56,10 +56,21 @@ def main():
     test_data = pd.read_csv(test_file)
 
     # Plot class distribution for binary_label
-    plot_class_distribution(train_data, val_data, test_data, 'binary_label', save_dir)
+    plot_class_distribution(
+                            train_data, 
+                            val_data, 
+                            test_data, 
+                            'binary_label', 
+                            save_dir)
 
     # Plot class distribution for multi_label
-    plot_class_distribution(train_data, val_data, test_data, 'multi_label', save_dir)
+    plot_class_distribution(
+                            train_data, 
+                            val_data, 
+                            test_data, 
+                            # 'multi_label', 
+                            'accepted_label',
+                            save_dir)
 
 if __name__ == "__main__":
     main()
